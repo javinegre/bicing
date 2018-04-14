5bfad9be2edd9b3053975f507553e5967f80da7a
Javier Negre <javi.negre@gmail.com>
2018-04-14 11:46:51 +0200

    Minor refactoring for Station Service

--- src/app/station.service.ts
+++ src/app/station.service.ts
@@ -9,20 +9,21 @@ import { HttpClient } from '@angular/common/http';
 
 import 'rxjs/add/operator/toPromise';
 
-let STATIONS = [];
-
 @Injectable()
 
 export class StationService {
     server: string = environment.bicingApiUrl;
     api: string = 'bicing/api/v1.0/';
 
+    stationList: Station[] = [];
+    updateTime: number;
+
     constructor(
         private http: HttpClient
     ) {}
 
     getStation (id): Promise<Station> {
-        return Promise.resolve(STATIONS.find( station => station.id === id ) || null);
+        return Promise.resolve(this.stationList.find( station => station.id === id ) || null);
     }
 
     getStations(simplifiedData: boolean = false): Promise<Station[]> {
@@ -32,25 +33,31 @@ export class StationService {
                 : 'stations';
 
             this.http.get(`${this.server}${this.api}${endpoint}`).toPromise().then((info: BicingApiResponse) => {
-                if ( !simplifiedData ) {
-                    STATIONS = info.stations;
+                if ( simplifiedData ) {
+                    info.stations = this.fillSimplifiedData(info.stations);
                 }
-                else {
-                    STATIONS.forEach((st) => {
-                        const newStationData = info.stations.find((it) => it.id == st.id);
-                        if ( newStationData ) {
-                            st.slots = newStationData.slots;
-                            st.bikes = newStationData.bikes;
-                            st.status = newStationData.status;
-                        }
-                    });
-                }
-                resolve(info.stations);
-            });
+                this.stationList = info.stations;
+                this.updateTime = info.updateTime;
+
+                resolve(this.stationList);
+            }).catch((err) => console.error(err));
         })
     }
 
     getStationsById(IDs): Promise<Station[]> {
-        return Promise.resolve(STATIONS.filter( station => IDs.indexOf(station.id) >= 0 ));
+        return Promise.resolve(this.stationList.filter( station => IDs.indexOf(station.id) >= 0 ));
+    }
+
+    fillSimplifiedData(stations): Station[] {
+        return this.stationList.map((st) => {
+            const newStationData = stations.find((it) => it.id == st.id);
+            if (newStationData) {
+                st.slots = newStationData.slots;
+                st.bikes = newStationData.bikes;
+                st.status = newStationData.status;
+            }
+
+            return st;
+        });
     }
 }
